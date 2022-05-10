13c7a4c8a5ebbc6ddb778833b065007dfd1bd1c3
Edwin Rozo Gómez <erozog125@gmail.com>
2022-05-09 21:45:07 -0500

    Esqueleto

---
+++ app.js
@@ -0,0 +1,16 @@
+const mainCards = document.querySelector("main");
+const selectProducts = document.getElementById("select-products");
+
+window.addEventListener('load', listSelect);
+selectProducts.addEventListener('change', renderCards);
+
+function renderCards() {  
+}
+
+function listSelect() {
+}
+
+
+function createCards(fruits) {
+  const {product, image,id,price} = fruits;  
+}
---
+++ products.js
@@ -0,0 +1,12 @@
+const fruits = [
+  {id:"fruit-01",product: 'apple',price: 0.5,image: '/Images/apple.jpeg'}, 
+  {id:"fruit-02",product: 'orange',price: 0.7,image: '/Images/orange.jpeg'},
+  {id:"fruit-03",product: 'banana',price: 0.8,image: '/Images/banana.jpeg'},
+  {id:"fruit-04",product: 'pineapple',price: 1.5,image: '/Images/pineapple.jpg'},
+  {id:"fruit-05",product: 'peach',price: 2,image: '/Images/peach.jpeg'},
+  {id:"fruit-06",product: 'grapes',price: 2.5,image: '/Images/grapes.jpeg'},
+  {id:"fruit-07",product: 'watermelon',price: 3,image: '/Images/watermelon.jpeg'},
+  {id:"fruit-08",product: 'strawberry',price: 3.5,image: '/Images/strawberry.jpeg'},
+  {id:"fruit-09",product: 'pear',price: 4,image: '/Images/pear.jpeg'},
+  {id:"fruit-10",product: 'kiwi',price: 4.5,image: '/Images/kiwi.jpeg'},
+];
